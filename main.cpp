895aee146a9145f720a9bdb05cc54881f8f21c02
samalamading141 <samalamading141@gmail.com>
2026-09-16 10:23:30 -0600

    Artist Class implemented

--- main.cpp
+++ main.cpp
@@ -3,8 +3,53 @@
 
 using namespace std;
 
-class Artist {
-// TODO
+class Artist
+{
+    private:
+        string artistName;
+        string artistGenre;
+        int artistFollowers;
+        double artistRating;
+    public:
+        Artist()
+        {
+            artistName = "None";
+            artistGenre = "None";
+            artistFollowers = 0;
+            artistRating = 0;
+        }
+
+        void Display()
+        {
+            cout << "Artist name: " << artistName << endl;
+            cout << "Artist genre: " << artistGenre << endl;
+            cout << "Total followers: " << artistFollowers << endl;
+            cout << "Artist rating: " << artistRating << endl;
+        }
+
+        void SetArtistName(string name)
+            { artistName = name; }
+
+        void SetArtistGenre(string genre)
+            { artistGenre = genre; }
+
+        void SetFollowers(int followers)
+            { artistFollowers = followers; }
+
+        void SetRating(double rating)
+            { artistRating = rating; }
+        
+        string GetArtistName()
+            { return artistName; }
+            
+        string GetArtistGenre()
+            { return artistGenre; }
+
+        int GetFollowers()
+            { return artistFollowers; }
+
+        double GetRating()
+            { return artistRating; } 
 }; 
 
 class Song {
